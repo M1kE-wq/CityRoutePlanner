e6686cc121544841dd5342ca7efe27f7e0ed4050
M1kE-wq <nikolaev.n.mihail@gmail.com>
2026-05-04 10:24:24 +0300

    Add Spring Boot main application class

---
+++ src/main/java/com/cityrouteplanner/CityRoutePlannerApplication.java
@@ -0,0 +1,15 @@
+package com.cityrouteplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the City Route Planner Spring Boot application.
+ */
+@SpringBootApplication
+public class CityRoutePlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityRoutePlannerApplication.class, args);
+    }
+}
